c71e427dfc36e8b8ab72aa7433d7fd77cf3ccdfe
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2024-01-31 19:19:52 -0300

    Listo responsive

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -12,15 +12,14 @@ export default function Spinner() {
         height="100vh"
         display="flex"
         flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
         position="relative"
+        className="divContent"
         >
       <Image 
         src="/assets/Logo_AscendantAllies (1).png" 
         alt="logo_AscendantAllies"
-        height="50%"/>
-      <Text fontWeight="bold" fontSize="30px"><span style={{ color: 'grey', fontWeight:"normal"}}> Ascendant</span> Allies</Text>
+        className="imageLogo"/>
+      <Text fontWeight="bold" className="title"><span style={{ color: 'grey', fontWeight:"normal"}}> Ascendant</span> Allies</Text>
       <Text>La pagina web esta bajo <span style={{ color: 'black', fontWeight: 'bold' }}>Mantenimiento</span></Text>
       <Text>En unos pocos dias <span style={{ color: 'black', fontWeight: 'bold' }}>Estaremos de vuelta</span></Text>
       </Box>
