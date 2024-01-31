a9ce8f9559b06848d97ce1ec1f1624b413feaba2
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2024-01-31 19:45:34 -0300

    added social media links

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -6,10 +6,18 @@ export default function SocialMedia() {
     return(
         <Box width="100%" height="100vh" position="absolute" display="flex" justifyContent="flex-end" alignItems="flex-end" padding="20px" zIndex="50">
         <Box display="flex" flexDirection="column" width="50px">
-            <FontAwesomeIcon icon={faLinkedin} color='blue'  size="2x"/>
-            <FontAwesomeIcon icon={faFacebook} color='blue'  size="2x"/>
-            <FontAwesomeIcon icon={faXTwitter} size="3x"/>
-            <FontAwesomeIcon icon={faInstagram} color='orange'/>
+            <a target='blank' href='https://www.linkedin.com/company/ascendant-allies/'>
+            <FontAwesomeIcon icon={faLinkedin} color='blue'  size="2x" />
+            </a>
+            <a target='blank' href='https://www.facebook.com/profile.php?id=61556158228843'>
+            <FontAwesomeIcon icon={faFacebook} color='blue'  size="2x" />
+            </a>
+            <a target='blank'  href='https://twitter.com/AscendantAllies'>
+            <FontAwesomeIcon icon={faXTwitter} size="3x" />
+            </a>
+            <a target='blank' href='https://www.instagram.com/ascendantallies/'>
+            <FontAwesomeIcon icon={faInstagram} color='orange' />
+            </a>
         </Box>
         </Box>
     )
