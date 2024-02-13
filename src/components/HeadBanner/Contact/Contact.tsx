86e6479585e1e08149db6745c3e537ee7a1c4c12
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2024-02-13 16:07:40 -0300

    listo enviar mensajes

--- src/components/HeadBanner/Contact/Contact.tsx
+++ src/components/HeadBanner/Contact/Contact.tsx
@@ -1,26 +1,65 @@
+"use client"
+import React,{useState} from 'react'
+import axios from "axios"
+
 export default function Contact(){
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = (e : any) =>{
+    const property = e.target.name;
+    const value = e.target.value
+
+    e.preventDefault();
+    setForm({...form, [property]: value})
+  }
+
+  const handleSubmit = async (e : any) => {
+        if(!form.name || !form.email || !form.message){
+            alert("Please complete the form correctly.");
+        }else{
+          try {
+            await axios.post('http://localhost:3001/', form);
+            alert('Email send successfully');
+          } catch (error) {
+            console.error('Error sending email', error);
+            alert('Error sending email');
+          }
+            setForm({
+                name: '',
+                email: '',
+                message: ''
+            })
+        }
+  }
     return(
-        <div className="w-full h-4/5 md:mx-24  flex justify-center items-center mt-10">
-            <form className="w-full md:w-3/5 border border-violetDawn p-10 md:rounded-2xl bg-violetDawn opacity-80">
+        <div className="w-full h-4/5 md:mx-24  flex justify-center items-center mt-28">
+            <form onSubmit={handleSubmit} className="w-full md:w-3/5 border border-violetDawn p-10 md:rounded-2xl bg-violetDawn opacity-80">
             <div>
               <label htmlFor="name" className="font-secondary text-2xl text-black opacity-100">Nombre:</label>
             </div>
             <div className=' w-full h-12 flex justify-center rounded-xl'>
-              <input  className='bg-eggshell w-full h-full text-2xl rounded-xl border-2 border-violetDawn  focus:outline-black font-secondary  text-black' type="text" name="name" placeholder="Tu nombre..."/> 
+              <input onChange={handleChange}  className='bg-eggshell w-full h-full text-2xl rounded-xl border-2 border-violetDawn  focus:outline-black font-secondary  text-black' type="text" name="name" placeholder="Tu nombre..."/> 
             </div>
 
             <div>
               <label htmlFor="email" className="font-secondary text-2xl text-black opacity-100">Correo:</label>
             </div>
             <div className=' w-full h-12 flex justify-center rounded-xl'>
-              <input  className='bg-eggshell w-full h-full text-2xl rounded-xl  border-2 border-violetDawn  focus:outline-black font-secondary  text-black' type="text" name="name" placeholder="Tu correo electronico..."/> 
+              <input onChange={handleChange} className='bg-eggshell w-full h-full text-2xl rounded-xl  border-2 border-violetDawn  focus:outline-black font-secondary  text-black' type="text" name="email" placeholder="Tu correo electronico..."/> 
             </div>
 
             <div>
               <label htmlFor="message" className="font-secondary text-2xl text-black opacity-100">Cuentanos sobre tu negocio y objetivos</label>
             </div>
             <div  className='w-full h-28 flex justify-center rounded-xl'>
-              <textarea className='bg-eggshell w-full h-full text-2xl rounded-xl border-2 border-violetDawn focus:outline-black font-secondary  text-black' name="message" />
+              <textarea onChange={handleChange} className='bg-eggshell w-full h-full text-2xl rounded-xl border-2 border-violetDawn focus:outline-black font-secondary  text-black' name="message" />
+            </div>
+            <div className="w-full flex justify-center items-center">
+            <button type="submit" className='h-20 w-64 hover:shadow-2xl bg-white rounded-xl my-4 flex items-center justify-center text-violetDawn hover:bg-black hover:text-white hover:cursor-pointer'>ENVIAR</button>  
             </div>
             </form>
         </div>
--- src/components/HeadBanner/Header/Header.tsx
+++ src/components/HeadBanner/Header/Header.tsx
@@ -15,12 +15,12 @@ export default function Header(){
                     <Image src="/assets/Logo_AscendantAllies_white.png" alt="logoWhite" width="100" height="100"/>
                      <h1 className="text-2xl md:text-3xl font-primary text-violetDawn">ASCENDANT <span className="text-white">ALLIES</span></h1> 
                 </div>}
-                <div  className="h-2/3 w-full mt-6 md:mt-2 flex flex-col justify-center items-center ">
-                    <h2 className="text-4xl md:text-6xl font-primary text-black ">BIENVENIDOS</h2>
+                <div  className="h-2/3 w-full mt-4 md:mt-2 flex flex-col justify-center items-center ">
+                    <h2 className="text-3xl md:text-6xl font-primary text-white ">BIENVENIDOS</h2>
                 {/* <p className="font-secondary text-xl md:text-lg text-white p-5 text-center">
                 Nos unimos con un propósito común: <span className="text-bold">Acercarnos hacia nuevas alturas juntos</span>. Somos una comunidad de colaboradores formados, creando alianzas poderosas para superar <span className="text-bold">desafíos</span> y alcanzar <span className="text-bold">metas ambiciosas.</span> Nuestro <span className="text-bold">compromiso</span> con el éxito no se limita solo a nosotros, sino que se extiende a cada miembro de nuestra red.
                 </p> */}
-                <p className="font-secondary text-2xl text-white p-5 md:p-0 md:pt-2 text-center ">
+                <p className="font-secondary text-xl md:text-2xl text-pattyPan px-5 md:p-0 md:pt-2 text-center ">
                 En <span className="font-bold">Ascendant Allies</span>, nos enorgullece ser tu aliado estratégico en la travesía hacia el <span className="font-bold">éxito empresarial</span>. Especializados en una gama completa de <span className="font-bold">servicios</span>, desde diseño <span className="font-bold">UX/UI</span> y <span className="font-bold">marketing</span> hasta<span className="font-bold"> arquitectura web</span> y más, ofrecemos soluciones integrales para impulsar tu <span className="font-bold">crecimiento</span>.
                 </p>
                 </div>
